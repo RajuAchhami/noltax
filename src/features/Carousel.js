9f07f2501d98518c63a45df5214c445a8abaccff
Raju Achhami <rajachhami55@gamil.com>
2024-06-11 10:02:43 +0545

    ch

--- src/features/Carousel.js
+++ src/features/Carousel.js
@@ -1,12 +1,35 @@
 import React from "react";
 import ChildComponent from "./ChildComponent";
+const defaultSlides = [
+  {
+    header: "OUR COURSES",
+    title: "With Scholar Teachers, Everything Is Easier",
+    desc: "Scholar is free CSS template designed by TemplateMo for online educational related websites. This layout is based on the famous Bootstrap v5.3.0 framework.",
+    btn: "What's Scholar?",
+    imgUrl: "/banner-item-01.jpg",
+  },
+  {
+    header: "BEST RESULT",
+    title: "Get the best result out of your effort",
+    desc: "You are allowed to use this template for any educational or commercial purpose. You are not allowed to re-distribute the template ZIP file on any other website.",
+    btn: "What's the best result?",
+    imgUrl: "/banner-item-02.jpg",
+  },
+  {
+    header: "ONLINE LEARNING",
+    title: "Online Learning helps you save the time",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporious incididunt ut labore et dolore magna aliqua suspendisse.",
+    btn: "What's Online Course  ?",
+    imgUrl: "/banner-item-03.jpg",
+  },
+];
 
 
 
 const Carousel = () => {
   return (
     <div className=" w-[60%] m-auto pt-11">
-      <ChildComponent  />
+      <ChildComponent slides={defaultSlides} />
     </div>
   );
 };
--- src/features/ChildComponent.js
+++ src/features/ChildComponent.js
@@ -1,35 +1,7 @@
 import React, { useState } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
-const defaultSlides = [
-  {
-    header: "OUR COURSES",
-    title: "With Scholar Teachers, Everything Is Easier",
-    desc: "Scholar is free CSS template designed by TemplateMo for online educational related websites. This layout is based on the famous Bootstrap v5.3.0 framework.",
-    btn: "What's Scholar?",
-    imgUrl: "/banner-item-01.jpg",
-  },
-  {
-    header: "BEST RESULT",
-    title: "Get the best result out of your effort",
-    desc: "You are allowed to use this template for any educational or commercial purpose. You are not allowed to re-distribute the template ZIP file on any other website.",
-    btn: "What's the best result?",
-    imgUrl: "/banner-item-02.jpg",
-  },
-  {
-    header: "ONLINE LEARNING",
-    title: "Online Learning helps you save the time",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporious incididunt ut labore et dolore magna aliqua suspendisse.",
-    btn: "What's Online Course  ?",
-    imgUrl: "/banner-item-03.jpg",
-  },
-];
-
-const ChildComponent = () => {
-  const [slides, setSlides] = useState(defaultSlides);
-
-  console.log(slides);
-
+const ChildComponent = ({ slides }) => {
   const [current, setCurrent] = useState(0);
 
   const prev = () => {
@@ -39,19 +11,18 @@ const ChildComponent = () => {
   };
 
   const next = () => {
-    
-    const isLastSlide = current === slides.length - 1;
-    if (isLastSlide)setSlides((prev) => {
-      return [...prev, ...defaultSlides];
-    });
-    const newIndex = isLastSlide ? 0 : current + 1;
-    setCurrent(newIndex);
+    if (current < slides.length - 1) {
+      setCurrent((prev) => prev + 1);
+    } else {
+      setCurrent(0);
+    }
   };
+
   return (
     <div className="overflow-hidden relative rounded-3xl">
       <div
-        className="flex transition duration-300 ease-in-out  "
-        style={{ transform: `translateX(-${current * 100}%)` }}
+        className="flex transition ease-in-out  duration-300"
+        style={{ transform: `translateX(-${current * 100}%` }}
       >
         {slides.map((s, i) => {
           return <img src={s.imgUrl} alt="" key={i} />;
